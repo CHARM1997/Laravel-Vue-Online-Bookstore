0131cd412fd753224d1cb748f1abb0eeeb063b98
it18208436 <it18208436@my.sliit.lk>
2022-10-21 00:36:48 +0530

    description add

--- laravel-cart/resources/views/products.blade.php
+++ laravel-cart/resources/views/products.blade.php
@@ -21,6 +21,7 @@
                         @csrf
                         <input type="hidden" value="{{ $product->id }}" name="id">
                         <input type="hidden" value="{{ $product->name }}" name="name">
+                        <input type="hidden" value="{{ $product->description }}" name="description">
                         <input type="hidden" value="{{ $product->price }}" name="price">
                         <input type="hidden" value="{{ $product->image }}"  name="image">
                         <input type="hidden" value="1" name="quantity">
